5381a3938f2564f0d1a710805c3be15402541908
Jhoel <jhoel.sv25@gmail.com>
2025-12-12 22:03:06 -0500

    Add product types definition for Product, ValidSizes, and ValidTypes

---
+++ types/product.types.ts
@@ -0,0 +1,21 @@
+
+export type Product = {
+  //id: string;
+  description: string;
+  images: string[];
+  inStock: number;
+  price: number;
+  sizes: ValidSizes[];
+  slug: string;
+  tags: string[];
+  title: string;
+  type: ValidTypes;
+  gender: "men" | "women" | "kid" | "unisex";
+}
+
+type ValidSizes = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
+type ValidTypes = "shirts" | "pants" | "hoodies" | "hats";
+
+
+export type ProductCategory = "men" | "women" | "kids";
+export type ProductSize = "S" | "M" | "L" | "XL";
